979d8b96d25f3a461ef2c958ab9247f2fac18892
Ogurtsov Artem <ogurcov.artem2012@list.ru>
2022-03-18 17:35:18 +0300

    connecting new swiper blocks / accordion betta

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,9 @@
 class Form {
   constructor(form) {
     this.form = form;
-    this.inputObserve();
+    if (this.form) {
+      this.inputObserve();
+    }
   }
   inputObserve() {
     [...this.form.elements].forEach((element) => {
@@ -27,73 +29,137 @@ class Form {
   }
 }
 
-const formTest = document.forms.designer;
-const designerFrom = new Form(formTest);
+const application = document.forms.application;
+const applicationFrom = new Form(application);
+const designer = document.forms.designer;
+const designerFrom = new Form(designer);
 
 class Accordion {
-  constructor(container, { duration }) {
+  constructor(container, { duration, onlyOne }) {
     this.container = container;
     this.duration = duration ? duration : 500;
+    this.onlyOne = onlyOne || true;
 
-    this.attributesName = {
-      item: "accordion-js-item",
-      button: "accordion-js-button",
-      body: "accordion-js-body",
-    };
-    this.classStatus = {
-      active: "accordion-opening-body",
-      open: "accordion-body-open",
+    this.techNames = {
+      attributesName: {
+        item: "data-accordion-js-item",
+        button: "data-accordion-js-button",
+        body: "data-accordion-js-body",
+      },
+      classStatus: {
+        activeBody: "accordion-opening-body",
+        openBody: "accordion-body-open",
+        activeButton: "accordion-button-active",
+      },
+      techAttribute: {
+        buttonId: "data-accordion-js-button-id",
+        bodyId: "data-accordion-js-body-id",
+      },
     };
     this.init();
   }
 
-  init() {
-    [...this.Items] = this.container.querySelectorAll(
-      `.${this.attributesName.item}`
-    );
-    this.Items.forEach((item) => {
-      this.setListener(item);
-    });
-
-    [...this.Bodies] = this.container.querySelectorAll(
-      `.${this.attributesName.body}`
-    );
-    this.Bodies.forEach((body) => {
-      this.slideUp(body);
-    });
+  _idGenerator() {
+    return "_" + Math.random().toString(36).substr(2, 9);
   }
 
-  removeActiveItems() {
-    [...this.activeItems] = this.container.querySelectorAll(
-      `.${this.attributesName.item}.${this.classStatus.open}`
-    );
-    if (this.activeItems) {
-      this.activeItems.forEach((activeItem) => {
-        this.slideUp(activeItem.querySelector(`.${this.attributesName.body}`));
-        activeItem.classList.remove(this.classStatus.open);
-      });
+  init() {
+    if (document.querySelector(this.container)) {
+      this.container = document.querySelector(this.container);
+      this.handler();
     }
   }
 
-  itemStatus(event) {
+  handler() {
+    this.getControls();
+    this.closeAll();
+    this.creatingUniquePairs();
+  }
+
+  getControls() {
+    const { attributesName } = this.techNames;
+    [...this.buttons] = this.container.querySelectorAll(
+      `[${attributesName.button}]`
+    );
+    [...this.bodies] = this.container.querySelectorAll(
+      `[${attributesName.body}]`
+    );
+  }
+  creatingUniquePairs() {
+    this.buttons.forEach((button, index) => {
+      const { techAttribute } = this.techNames;
+      let id = this._idGenerator();
+      button.setAttribute(techAttribute.buttonId, id);
+      if (this.bodies[index]) {
+        this.bodies[index].setAttribute(techAttribute.bodyId, id);
+      }
+      this.setListener(button);
+    });
+  }
+  eventHandler(event) {
     event.preventDefault();
-    const { currentTarget } = event;
-    let body = currentTarget.querySelector(`.${this.attributesName.body}`);
-    if (!body.classList.contains(this.classStatus.active)) {
-      this.removeActiveItems();
-      if (currentTarget.classList.contains(this.classStatus.open)) {
-        currentTarget.classList.remove(this.classStatus.open);
+
+    const { techAttribute, classStatus } = this.techNames;
+
+    const { target } = event;
+
+    const currentId = target.getAttribute(techAttribute.buttonId);
+    const button = target;
+    const body = this.container.querySelector(
+      `[${techAttribute.bodyId}="${currentId}"]`
+    );
+
+    if (!this.container.classList.contains(classStatus.activeBody)) {
+      this.statusController(body, button);
+      this.lastBody = body;
+      this.lastButton = button;
+    }
+  }
+  statusController(body, button) {
+    if (this.lastBody !== body && this.lastBody) {
+      this.slideUp(this.lastBody);
+      this.removeClass(this.lastBody, this.lastButton);
+      this.classController(body, button);
+    } else {
+      this.classController(body, button);
+    }
+  }
+
+  classController(body, button) {
+    const { classStatus } = this.techNames;
+    if (!body.classList.contains(classStatus.activeBody)) {
+      if (body.classList.contains(classStatus.openBody)) {
+        this.removeClass(body, button);
         this.slideUp(body);
       } else {
-        currentTarget.classList.add(this.classStatus.open);
+        this.addClass(body, button);
         this.slideDown(body);
       }
     }
   }
 
+  closeAll() {
+    this.bodies.forEach((body) => {
+      this.slideUp(body);
+    });
+  }
+  addClass(body, button) {
+    const { classStatus } = this.techNames;
+    body ? body.classList.add(classStatus.openBody) : null;
+    button ? button.classList.add(classStatus.activeButton) : null;
+  }
+
+  removeClass(body, button) {
+    const { classStatus } = this.techNames;
+    body ? body.classList.remove(classStatus.openBody) : null;
+    button ? button.classList.remove(classStatus.activeButton) : null;
+  }
+
   slideUp(target) {
-    if (!target.classList.contains(this.classStatus.active)) {
-      target.classList.add(this.classStatus.active);
+    const { classStatus } = this.techNames;
+    if (!target.classList.contains(classStatus.activeBody)) {
+      target.classList.add(classStatus.activeBody);
+      this.container.classList.add(classStatus.activeBody);
       target.style.transitionProperty = "height, margin, padding";
       target.style.transitionDuration = `${this.duration}ms`;
       target.style.height = `${target.offsetHeight}px`;
@@ -115,14 +181,17 @@ class Accordion {
         target.style.removeProperty("overflow");
         target.style.removeProperty("transition-duration");
         target.style.removeProperty("transition-property");
-        target.classList.remove(this.classStatus.active);
+        target.classList.remove(classStatus.activeBody);
+        this.container.classList.remove(classStatus.activeBody);
       }, this.duration);
     }
   }
 
   slideDown(target) {
-    if (!target.classList.contains(this.classStatus.active)) {
-      target.classList.add(this.classStatus.active);
+    const { classStatus } = this.techNames;
+    if (!target.classList.contains(classStatus.activeBody)) {
+      target.classList.add(classStatus.activeBody);
+      this.container.classList.add(classStatus.activeBody);
       if (target.hidden) {
         target.hidden = false;
       }
@@ -147,22 +216,96 @@ class Accordion {
         target.style.removeProperty("overflow");
         target.style.removeProperty("transition-duration");
         target.style.removeProperty("transition-property");
-        target.classList.remove(this.classStatus.active);
+        target.classList.remove(classStatus.activeBody);
+        this.container.classList.remove(classStatus.activeBody);
       }, this.duration);
     }
   }
 
   setListener(element) {
-    element.addEventListener("click", this.itemStatus.bind(this));
+    element.addEventListener("click", this.eventHandler.bind(this));
   }
 }
-const accordionTest = document.querySelector(".benefits__accordion_column-one");
-const accordionTest2 = document.querySelector(
-  ".benefits__accordion_column-two"
-);
-const accordion = new Accordion(accordionTest, {
+
+const accordion2 = new Accordion(".benefits__body", {
   duration: 500,
 });
-const accordion2 = new Accordion(accordionTest2, {
+const accordion3 = new Accordion(".filter-selector", {
   duration: 500,
+});
+
+const kitchenSmallSwiper = new Swiper(".kitchen-large__swiper", {
+  loop: true,
+  observer: true,
+  observerParents: true,
+  slidesPerView: 1,
+  spaceBetween: 32,
+  speed: 800,
+  loop: true,
+  loopAdditionalSlides: 5,
+  preloadImages: false,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".kitchen-large__controls_right",
+    prevEl: ".kitchen-large__controls_left",
+  },
+});
+
+const kitchenLargeSwiper = new Swiper(".kitchen-small__swiper", {
+  loop: true,
+
+  slidesPerView: 1,
+  spaceBetween: 32,
+  speed: 800,
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".kitchen-small__controls_right",
+    prevEl: ".kitchen-small__controls_left",
+  },
+});
+
+const kitchenFullSwiper = new Swiper(".kitchen-full__swiper", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 32,
+  speed: 800,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".kitchen-full__controls_right",
+    prevEl: ".kitchen-full__controls_left",
+  },
+});
+
+const gallaryDetail = new Swiper(".detail-gallary__swiper", {
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  freeMode: true,
+});
+const popularKit = new Swiper(".popular-kit__swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+  breakpoints: {
+    375: {
+      slidesPerView: 1.3,
+      spaceBetween: 10,
+    },
+    650: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+  navigation: {
+    nextEl: ".popular-kit__button_right",
+    prevEl: ".popular-kit__button_left",
+  },
 });
